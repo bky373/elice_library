5e1ad13e5496820728a4549126e939eee17648ca
bky373 <bo373@naver.com>
2021-02-23 17:57:15 +0900

    modify max length of password and add serialized method to User model

--- elice_library/models.py
+++ elice_library/models.py
@@ -4,12 +4,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(32), unique=True, nullable=False)
-    password_hash = db.Column(db.String(32), nullable=False)
+    username = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @property
+    def serialized(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+        }
