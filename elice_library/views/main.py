52942c285c4cc1af445992d839f7bfe97809a43c
bky373 <bo373@naver.com>
2021-02-23 21:14:22 +0900

    add index template with style

--- elice_library/views/main.py
+++ elice_library/views/main.py
@@ -1,9 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @main_bp.route('/')
 def index():
-    return 'Welcome to Borahm Library :) !!!!'
-
+    return render_template('base.html')
