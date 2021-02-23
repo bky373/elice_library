94e5e65fbd7c480a12cba13d2c1d86bb1056775f
bky373 <bo373@naver.com>
2021-02-23 16:53:28 +0900

    add password methods on User model

--- elice_library/models.py
+++ elice_library/models.py
@@ -1,9 +1,15 @@
 from elice_library import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(32), unique=True, nullable=False)
-    password = db.Column(db.String(32), nullable=False)
-    
+    password_hash = db.Column(db.String(32), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
