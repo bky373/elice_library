74ed3aabb578c825fdcfbde63656de46973aab5e
bky373 <bo373@naver.com>
2021-02-24 21:19:52 +0900

    move db uri and secret key to .env file

--- config.py
+++ config.py
@@ -3,14 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_URL = 'mysql+pymysql://{user}:{pw}@{host}:{port}/{db}?charset=utf8'.format(
-    user=os.getenv('DB_USER'),
-    pw=os.getenv('DB_PASS'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    db=os.getenv('DB_NAME')
-)
 
-SECRET_KEY = 'wlqdlsepwlqrkrhtlvek' # TODO 추후 변경
-SQLALCHEMY_DATABASE_URI = DB_URL
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
