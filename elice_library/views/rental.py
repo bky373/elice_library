77abcdefd3434bf7fae6a03014e44b81a4853211
bky373 <bo373@naver.com>
2021-02-25 22:47:59 +0900

    add books_return page and feature of returning a book

--- elice_library/views/rental.py
+++ elice_library/views/rental.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, render_template, redirect, url_for, session
 from elice_library.models import User, Book, BookRental
 from elice_library import db
+from datetime import datetime
 
 rental_bp = Blueprint('rental', __name__, url_prefix='/rental')
 
 
-@rental_bp.route('/', methods=('GET', 'POST' ))
+@rental_bp.route('/', methods=('GET', 'POST'))
 def info():
     user_id = session['user_id']
     user = User.query.filter_by(id=user_id).first()
@@ -28,3 +29,19 @@ def info():
         return redirect(url_for('main.index'))
     return render_template('rental/rental_info.html', rental_infos=user.rental_set)
 
+
+@rental_bp.route('/books-return', methods=('GET', 'POST'))
+def books_return():
+    user_id = session['user_id']
+    user = User.query.filter_by(id=user_id).first()
+
+    if request.method == 'POST':
+        book_id = request.form.get('book')
+        book = Book.query.filter_by(id=book_id).first()
+        book.stock_num += 1
+        rental_info = BookRental.query.filter_by(user=user, book=book).first()
+        rental_info.returned_at = datetime.now()
+        db.session.commit()
+        return redirect(url_for('rental.info'))
+    rental_infos = [info for info in user.rental_set if not info.returned_at]
+    return render_template('rental/books_return.html', rental_infos=rental_infos)
