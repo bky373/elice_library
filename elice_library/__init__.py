a40619909510bf0029c9d562f390301148888d7a
bky373 <bo373@naver.com>
2021-02-25 18:58:56 +0900

    add renting a book feature

--- elice_library/__init__.py
+++ elice_library/__init__.py
@@ -12,9 +12,11 @@ def register_blueprints(app):
     from .views.main import main_bp
     from .views.auth import auth_bp
     from .views.books import books_bp
+    from .views.rental import rental_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(books_bp)
+    app.register_blueprint(rental_bp)
 
 
 def create_app():
