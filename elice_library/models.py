04d918a1aeff1c5a57b546ed6db542c1ae582147
bky373 <bo373@naver.com>
2021-02-25 16:05:49 +0900

    update Book fields with published_at and image_path

--- elice_library/models.py
+++ elice_library/models.py
@@ -23,19 +23,20 @@ class Book(db.Model):
     book_name = db.Column(db.String(128), nullable=False)
     publisher = db.Column(db.String(128), nullable=False)
     author = db.Column(db.String(128), nullable=False)
-    publication_date = db.Column(db.DateTime, nullable=False)
+    published_at = db.Column(db.DateTime, nullable=False)
     pages = db.Column(db.Integer, nullable=False)
     isbn = db.Column(db.String(128), nullable=False)
     description = db.Column(db.Text, nullable=False)
     link = db.Column(db.Text, nullable=False)
+    image_path = db.Column(db.Text)
     stock_num = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Integer, nullable=False, default=0)
 
 
 class BookSchema(ma.Schema):
     class Meta:
-        fields = ("id", "book_name", "publisher", "author", "publication_date",
-                  "pages", "isbn", "description", "link", "stock_num", "rating")
+        fields = ("id", "book_name", "publisher", "author", "published_at",
+                  "pages", "isbn", "description", "link", "image_path", "stock_num", "rating")
 
 
 def must_not_be_blank(data):
--- load_data.py
+++ load_data.py
@@ -1,24 +1,36 @@
-from elice_library.models import Book
-from elice_library import db
 import csv
+from datetime import date, datetime
 
+from elice_library import db, create_app
+from elice_library.models import Book
 
-def init_books():
-    with open('library_data.csv', newline='', encoding='UTF8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
+app = create_app()
+with app.app_context():
+    with open('library_data.csv', 'r', encoding='UTF8') as csvfile:
+        reader = csv.DictReader(csvfile)
         for row in reader:
-            if row[0] != ' ':
-                book = Book(
-                    book_name=row[1],
-                    publisher=row[2],
-                    author=row[3],
-                    publication_date=row[4],
-                    pages=int(row[5]),
-                    isbn=int(row[6]),
-                    description=row[7],
-                    link=row[8],
-                    stock_num=1,
-                    rating=3
-                )
-                db.session.add(book)
-                db.session.commit()
+            published_at = datetime.strptime(
+                row['publication_date'], '%Y-%m-%d'
+            ).date()
+            image_path = f"/static/images/{row['id']}"
+            try :
+                open(f'elice_library/{image_path}.png')
+                image_path += '.png'
+            except:
+                image_path += '.jpg'
+
+            book = Book(
+                book_name=row['book_name'], 
+                publisher=row['publisher'],
+                author=row['author'], 
+                published_at=published_at, 
+                pages=int(row['pages']),
+                isbn=row['isbn'], 
+                description=row['description'], 
+                link=row['link'],
+                image_path=image_path,
+                stock_num=1,
+                rating=3
+            )
+            db.session.add(book)
+        db.session.commit()
