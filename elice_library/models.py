4ba329eb600f83f8fb4d9a273c0585d212b42742
bky373 <bo373@naver.com>
2021-02-25 17:35:29 +0900

    add BookRental initial model

--- elice_library/models.py
+++ elice_library/models.py
@@ -33,6 +33,16 @@ class Book(db.Model):
     rating = db.Column(db.Integer, nullable=False, default=0)
 
 
+class BookRental(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id', ondelete='CASCADE'))
+    book = db.relationship('Book', backref=db.backref('rental_set'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref("rental_set"))
+    rented_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    returned_at = db.Column(db.DateTime, nullable=True)
+
+
 class BookSchema(ma.Schema):
     class Meta:
         fields = ("id", "book_name", "publisher", "author", "published_at",
