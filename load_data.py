2a574f57b9a4eddca110922328e7933b7179561a
bky373 <bo373@naver.com>
2021-02-28 20:38:26 +0900

    move library_data and load_data file out of utils

--- load_data.py
+++ load_data.py
@@ -1,33 +1,34 @@
 import csv
 from datetime import date, datetime
 
-from elice_library import db, create_app
+from elice_library import create_app
+from elice_library.database.config import db
 from elice_library.database.models.book import Book
 
 
 app = create_app()
 with app.app_context():
-    with open('utils/library_data.csv', 'r', encoding='UTF8') as csvfile:
+    with open('library_data.csv', 'r', encoding='UTF8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             published_at = datetime.strptime(
                 row['publication_date'], '%Y-%m-%d'
             ).date()
             image_path = f"/static/images/{row['id']}"
-            try :
+            try:
                 open(f'elice_library/{image_path}.png')
                 image_path += '.png'
             except:
                 image_path += '.jpg'
 
             book = Book(
-                book_name=row['book_name'], 
+                book_name=row['book_name'],
                 publisher=row['publisher'],
-                author=row['author'], 
-                published_at=published_at, 
+                author=row['author'],
+                published_at=published_at,
                 pages=int(row['pages']),
-                isbn=row['isbn'], 
-                description=row['description'], 
+                isbn=row['isbn'],
+                description=row['description'],
                 link=row['link'],
                 image_path=image_path,
                 stock_num=1,
