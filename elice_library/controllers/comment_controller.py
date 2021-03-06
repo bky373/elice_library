5e371806db8d000f915e6a876780e8f45770a2ae
bky373 <bo373@naver.com>
2021-03-06 14:12:22 +0900

    make comment_service function module from class

--- elice_library/controllers/comment_controller.py
+++ elice_library/controllers/comment_controller.py
@@ -3,7 +3,7 @@ from flask_restx import Namespace
 from marshmallow import ValidationError
 from elice_library.domain.schemas.comment_schema import CommentSchema
 from elice_library.services.book_service import sort_books_by_comments_num
-from elice_library.services.comment_service import CommentService
+from elice_library.services.comment_service import create_comment, update_comment, delete_comment, get_comments_by_book_id
 from elice_library.controllers.auth_controller import Resource
 from elice_library.utils.errors import (
     CommentAlreadyPostedError,
@@ -16,7 +16,6 @@ from elice_library.utils.errors import (
 api = Namespace("comment", description="comment related operations")
 
 comment_schema = CommentSchema(many=True)
-comment_service = CommentService()
 
 COMMENT_UPDATE_SUCCESS = "댓글을 수정하였습니다."
 COMMENT_DELETE_SUCCESS = "댓글을 삭제하였습니다."
@@ -29,21 +28,22 @@ class Comment(Resource):
             data = request.get_json()
             content, rating, book_id = data["content"], data["rating"], data["book_id"]
 
-            comment_service.create_comment(g.user.id, book_id, content, rating)
+            create_comment(g.user.id, book_id, content, rating)
 
         except CommentAlreadyPostedError as e:
             return {"message": e.message}, 400
         except ValidationError as e:
             return {"message": e.messages}, 400
         return {
-            "comments": comment_schema.dump(comment_service.get_comments_by(book_id))
+            "comments": comment_schema.dump(get_comments_by_book_id(book_id))
         }
 
     def put(self):
         try:
             data = request.get_json()
             comment_id, content = data["comment_id"], data["content"]
-            comment_service.update_comment(comment_id, content)
+            
+            update_comment(comment_id, content)
 
         except CommentNotExistError as e:
             return {"message": e.message}, 400
@@ -59,7 +59,7 @@ class Comment(Resource):
             data = request.get_json()
             comment_id = data["comment_id"]
 
-            comment_service.delete_comment(comment_id)
+            delete_comment(comment_id)
 
         except CommentNotExistError as e:
             return {"message": e.message}, 400
--- elice_library/services/comment_service.py
+++ elice_library/services/comment_service.py
@@ -11,50 +11,49 @@ from elice_library.utils.errors import (
 )
 
 
-class CommentService:
-    def find_by_id(self, comment_id) -> Comment:
-        return Comment.query.filter_by(id=comment_id).first()
+def get_comment_by_id(comment_id) -> Comment:
+    return Comment.query.filter_by(id=comment_id).first()
 
-    def find_last_by_userid_and_bookid(self, user_id, book_id) -> Comment:
-        return Comment.query.filter_by(user_id=user_id, book_id=book_id).first()
+def get_comment_by_userid_and_bookid(user_id, book_id) -> Comment:
+    return Comment.query.filter_by(user_id=user_id, book_id=book_id).first()
 
-    def create_comment(self, user_id, book_id, content, rating) -> Comment:
-        user = get_user_by_id(user_id)
-        book = get_book_by_id(book_id)
+def create_comment(user_id, book_id, content, rating) -> Comment:
+    user = get_user_by_id(user_id)
+    book = get_book_by_id(book_id)
 
-        existed = self.find_last_by_userid_and_bookid(user_id, book_id)
-        if existed:
-            raise CommentAlreadyPostedError()
+    existed = get_comment_by_userid_and_bookid(user_id, book_id)
+    if existed:
+        raise CommentAlreadyPostedError()
 
-        if not content:
-            raise ValidationError(COMMENT_REQUIRED)
-        if not rating:
-            raise ValidationError(SCORE_REQUIRED)
+    if not content:
+        raise ValidationError(COMMENT_REQUIRED)
+    if not rating:
+        raise ValidationError(SCORE_REQUIRED)
 
-        comment = Comment.create(user, book, content, rating)
-        book.update_rating()
+    comment = Comment.create(user, book, content, rating)
+    book.update_rating()
 
-        self.save_to_db(comment)
-        return comment
+    save_to_db(comment)
+    return comment
 
-    def get_comments_by(self, book_id) -> List[Comment]:
-        book = get_book_by_id(book_id)
-        return book.comments
+def get_comments_by_book_id(book_id) -> List[Comment]:
+    book = get_book_by_id(book_id)
+    return book.comments
 
-    def update_comment(self, comment_id, content) -> Comment:
-        if not content:
-            raise ValidationError(COMMENT_REQUIRED)
+def update_comment(comment_id, content) -> Comment:
+    if not content:
+        raise ValidationError(COMMENT_REQUIRED)
 
-        comment = self.find_by_id(comment_id)
-        comment.update(content)
-        self.save_to_db(comment)
-        return comment
+    comment = get_comment_by_id(comment_id)
+    comment.update(content)
+    save_to_db(comment)
+    return comment
 
-    def delete_comment(self, comment_id) -> None:
-        comment = self.find_by_id(comment_id)
-        db.session.delete(comment)
-        db.session.commit()
+def delete_comment(comment_id) -> None:
+    comment = get_comment_by_id(comment_id)
+    db.session.delete(comment)
+    db.session.commit()
 
-    def save_to_db(self, user) -> None:
-        db.session.add(user)
-        db.session.commit()
+def save_to_db(user) -> None:
+    db.session.add(user)
+    db.session.commit()
